9753fef1629f01c8d03a7f0aab1f6127d91d2947
Lakhani Mohammad Raza <razalakhani21@gmail.com>
2024-08-26 15:51:43 +0530

    Now make some changes to make your life easier with React use the already made Array of Data, so we do not have to 'Hard-Code', stay tuned after this we will be using 'Array map() and Fat Arrow funtion' in this project

--- Netflix-WebSeries's-Card-App/src/App.jsx
+++ Netflix-WebSeries's-Card-App/src/App.jsx
@@ -1,18 +1,42 @@
 import "./App.css";
-import Cards from './Components/Cards'
+import Cards from "./Components/Cards";
+import Sdata from "./Components/Sdata";
 
 function App() {
-
   return (
     <>
+      <h1 className="heading_style">List of Top 3 Netlix Series</h1>
 
-    <h1 className='heading_style'>List of Top 3 Netlix Series</h1>
-
-    <Cards sname="DARK" imgsrc="https://wallpapercave.com/uwp/uwp4471004.png" title= "Netflix Original Series" links= "https://www.netflix.com/in/title/80990668?source=35"/>
-
-    <Cards sname="Stranger Things" imgsrc="https://wallpapercave.com/uwp/uwp4222465.jpeg" title= "Netflix Original Series" links= "https://www.netflix.com/in/title/80990668?source=35"/>
-
-    <Cards sname="Extra Curricular" imgsrc="https://wallpapercave.com/wp/wp11171928.jpg" title= "Netflix Original Series" links= "https://www.netflix.com/in/title/80990668?source=35"/>
+      <Cards
+        sname={Sdata[0].sname}
+        imgsrc={Sdata[0].imgsrc}
+        title={Sdata[0].title}
+        links={Sdata[0].links}
+      />
+      <Cards
+        sname={Sdata[1].sname}
+        imgsrc={Sdata[1].imgsrc}
+        title={Sdata[1].title}
+        links={Sdata[1].links}
+      />
+      <Cards
+        sname={Sdata[2].sname}
+        imgsrc={Sdata[2].imgsrc}
+        title={Sdata[2].title}
+        links={Sdata[2].links}
+      />
+      <Cards
+        sname={Sdata[3].sname}
+        imgsrc={Sdata[3].imgsrc}
+        title={Sdata[3].title}
+        links={Sdata[3].links}
+      />
+      <Cards
+        sname={Sdata[4].sname}
+        imgsrc={Sdata[4].imgsrc}
+        title={Sdata[4].title}
+        links={Sdata[4].links}
+      />
     </>
   );
 }
---
+++ Netflix-WebSeries's-Card-App/src/Components/Sdata.jsx
@@ -0,0 +1,35 @@
+const Sdata = [
+  {
+    sname: "DARK",
+    imgsrc: "https://wallpapercave.com/uwp/uwp4471004.png",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    sname: "extra curricular",
+    imgsrc: "https://wallpapercave.com/uwp/uwp4222465.jpeg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    sname: "Stranger Things",
+    imgsrc: "https://wallpapercave.com/wp/wp11171928.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    sname: "the Vampire Diaries",
+    imgsrc: "https://wallpapercave.com/uwp/uwp4473202.jpeg",
+    title: "Amazon Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    sname: "My First-2 Love",
+    imgsrc:
+      "https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/81026700?source=35",
+  },
+];
+
+export default Sdata;
